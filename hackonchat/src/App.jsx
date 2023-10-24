8d84e2b9743b1144fa11c441b309f000d49893c4
Jayan Pahuja <jayanpahuja@Jayans-MacBook-Pro.local>
2023-10-24 23:58:05 +0530

    Added chatbot

---
+++ hackonchat/src/App.jsx
@@ -0,0 +1,14 @@
+
+import './App.css';
+import MessageContainer from './components/messagecontainer/messageContainer';
+function App() {
+ 
+
+  return (
+    <div className="App">
+      <MessageContainer/>
+    </div>
+  );
+}
+
+export default App;
